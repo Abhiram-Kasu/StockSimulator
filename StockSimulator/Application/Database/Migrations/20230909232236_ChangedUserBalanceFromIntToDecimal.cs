a1084e61de10664012a735cf5c7a5303603154dc
SoapDispenser <abhiram.kasu@gmail.com>
2023-09-16 21:42:31 -0600

    Fixed style error on buy stocks page, still need to fix error with dashboard not updating once stock is bought

---
+++ StockSimulator/Application/Database/Migrations/20230909232236_ChangedUserBalanceFromIntToDecimal.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StockSimulator.Application.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedUserBalanceFromIntToDecimal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "balance",
+                table: "users",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "balance",
+                table: "users",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT");
+        }
+    }
+}
---
+++ StockSimulator/Application/Database/Migrations/20230910052845_TryFixRelationIssuesBetweenStocksAndUsers.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StockSimulator.Application.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class TryFixRelationIssuesBetweenStocksAndUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
